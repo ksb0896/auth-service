6baf10026f89bbc34ec87189c6044069ece003db
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-12-30 23:00:16 +0530

    removed commented code

--- src/main/java/com/ksb/micro/auth_service/controller/AuthController.java
+++ src/main/java/com/ksb/micro/auth_service/controller/AuthController.java
@@ -17,15 +17,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 //DTOs for request/response
 record LoginRequest(@NotBlank String username, @NotBlank @Size(min = 4, max = 6) String password){}
