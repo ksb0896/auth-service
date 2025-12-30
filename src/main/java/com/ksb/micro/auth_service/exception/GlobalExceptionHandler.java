e3245e1d6a75485f1f3371ef642d515e319065d4
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-12-30 22:57:33 +0530

    added global exception handling for bad creds

--- src/main/java/com/ksb/micro/auth_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ksb/micro/auth_service/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ksb.micro.auth_service.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,5 +23,12 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleLoginError(BadCredentialsException ex) {
+        Map<String, String> errorBody = new HashMap<>();
+        errorBody.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorBody);
     }
 }
